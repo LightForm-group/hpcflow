d540d2a4dc7c949ec3fd5c85c89323c3b00cd8b1
aplowman <adam.jp@live.co.uk>
2019-06-14 21:34:08 +0100

    Update OS classifier

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Operating System :: OS Independent',
+        'Operating System :: POSIX',
     ],
     entry_points="""
         [console_scripts]
