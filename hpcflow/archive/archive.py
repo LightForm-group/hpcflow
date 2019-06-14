05bd1d013fa06e920e009521ee604ff3accc208f
aplowman <adam.jp@live.co.uk>
2019-06-14 21:12:41 +0100

    Move archive class to own module

---
+++ hpcflow/archive/archive.py
@@ -0,0 +1,138 @@
+"""`hpcflow.archive.archive.py`
+
+This module contains a database model class for the archiving capabilities.
+
+"""
+
+import shutil
+from datetime import datetime
+from pathlib import Path
+from pprint import pprint
+from time import sleep
+from datetime import datetime
+from shutil import ignore_patterns
+
+from sqlalchemy import (Table, Column, Integer, DateTime, ForeignKey, String,
+                        UniqueConstraint)
+from sqlalchemy.orm import relationship, Session
+from sqlalchemy.exc import IntegrityError, OperationalError
+
+from hpcflow import CONFIG
+from hpcflow.base_db import Base
+from hpcflow.copytree import copytree_multi
+
+archive_is_active = Table(
+    'archive_is_active',
+    Base.metadata,
+    Column(
+        'archive_id',
+        Integer,
+        ForeignKey('archive.id'),
+        primary_key=True
+    ),
+    Column(
+        'directory_value_id',
+        Integer,
+        ForeignKey('var_value.id'),
+        primary_key=True
+    ),
+)
+
+
+class Archive(Base):
+    """Class to represent an archive location."""
+
+    __tablename__ = 'archive'
+    __table_args__ = (
+        UniqueConstraint('path', 'host', name='archive_location'),
+    )
+
+    id_ = Column('id', Integer, primary_key=True)
+    name = Column(String(255))
+    path = Column(String(255))
+    host = Column(String(255))
+
+    command_groups = relationship('CommandGroup', back_populates='archive')
+    directories_archiving = relationship(
+        'VarValue',
+        secondary=archive_is_active
+    )
+
+    def __init__(self, name, path, host=''):
+
+        self.name = name
+        self.path = path
+        self.host = host
+
+    def execute(self, directory_value, exclude):
+        """Execute the archive process of a given working directory.
+
+        Parameters
+        ----------
+        directory_value : VarValue
+        exclude : list of str
+
+        """
+
+        root_dir = self.command_groups[0].workflow.directory
+        src_dir = root_dir.joinpath(directory_value.value)
+        dst_dir = Path(self.path).joinpath(directory_value.value)
+
+        session = Session.object_session(self)
+
+        wait_time = 10
+        blocked = True
+        while blocked:
+            print('{} Archiving blocked...'.format(datetime.now()), flush=True)
+            session.refresh(self)
+
+            if directory_value in self.directories_archiving:
+                sleep(wait_time)
+            else:
+                try:
+                    self.directories_archiving.append(directory_value)
+                    session.commit()
+                    blocked = False
+
+                except IntegrityError:
+                    # Another process has already set `directories_archiving`
+                    session.rollback()
+                    sleep(wait_time)
+
+                except OperationalError:
+                    # Database is likely locked.
+                    session.rollback()
+                    sleep(wait_time)
+
+                if not blocked:
+                    # Need to ensure the block is released if copying fails:
+                    try:
+                        self._copy(src_dir, dst_dir, exclude)
+                    except shutil.Error as err:
+                        print('Archive copying error: {}'.format(err))
+
+                    self.directories_archiving.remove(directory_value)
+                    session.commit()
+
+    def _copy(self, src_dir, dst_dir, exclude):
+        """Do the actual copying.
+
+        TODO: does copytree overwrite all files or just copy
+        non-existing files?
+
+        TODO: later (safely) copy the database to archive as well?
+
+        """
+
+        ignore = [CONFIG['hpcflow_directory']] + (exclude or [])
+
+        if ignore:
+            ignore_func = ignore_patterns(*ignore)
+        else:
+            ignore_func = None
+
+        start = datetime.now()
+        copytree_multi(str(src_dir), str(dst_dir), ignore=ignore_func)
+        end = datetime.now()
+        copy_seconds = (end - start).total_seconds()
+        print('Archive took {} seconds'.format(copy_seconds), flush=True)
--- hpcflow/models.py
+++ hpcflow/models.py
@@ -5,46 +5,25 @@ import re
 import os
 from datetime import datetime
 from pathlib import Path
-from math import ceil, floor
 from pprint import pprint
 from subprocess import run, PIPE
 from time import sleep
 from datetime import datetime
-from shutil import ignore_patterns
 
-from sqlalchemy import (
-    Table, Column, Integer, DateTime, JSON, ForeignKey, Boolean, Enum, String,
-    select, UniqueConstraint
-)
+from sqlalchemy import (Column, Integer, DateTime, JSON, ForeignKey, Boolean,
+                        Enum, String, select)
 from sqlalchemy.orm import relationship, deferred, Session
 from sqlalchemy.exc import IntegrityError, OperationalError
 
 from hpcflow import CONFIG
+from hpcflow.archive.archive import Archive
 from hpcflow.base_db import Base
 from hpcflow.nesting import NestingType
+from hpcflow.utils import coerce_same_length, zeropad
 from hpcflow.validation import validate_task_multiplicity
 from hpcflow.variables import (
     select_cmd_group_var_names, select_cmd_group_var_definitions,
     extract_variable_names, resolve_variable_values, UnresolvedVariableError
-)
-from hpcflow.utils import coerce_same_length, zeropad
-from hpcflow.copytree import copytree_multi
-
-archive_is_active = Table(
-    'archive_is_active',
-    Base.metadata,
-    Column(
-        'archive_id',
-        Integer,
-        ForeignKey('archive.id'),
-        primary_key=True
-    ),
-    Column(
-        'directory_value_id',
-        Integer,
-        ForeignKey('var_value.id'),
-        primary_key=True
-    ),
 )
 
 
@@ -1550,104 +1529,6 @@ class VarValue(Base):
         self.variable_definition = var_definition
         self.submission = submission
         self.directory_value = directory_value
-
-
-class Archive(Base):
-    """Class to represent an archive location."""
-
-    __tablename__ = 'archive'
-    __table_args__ = (
-        UniqueConstraint('path', 'host', name='archive_location'),
-    )
-
-    id_ = Column('id', Integer, primary_key=True)
-    name = Column(String(255))
-    path = Column(String(255))
-    host = Column(String(255))
-
-    command_groups = relationship('CommandGroup', back_populates='archive')
-    directories_archiving = relationship(
-        'VarValue',
-        secondary=archive_is_active
-    )
-
-    def __init__(self, name, path, host=''):
-
-        self.name = name
-        self.path = path
-        self.host = host
-
-    def execute(self, directory_value, exclude):
-        """Execute the archive process of a given working directory.
-
-        Parameters
-        ----------
-        directory_value : VarValue
-        exclude : list of str
-
-        """
-
-        root_dir = self.command_groups[0].workflow.directory
-        src_dir = root_dir.joinpath(directory_value.value)
-        dst_dir = Path(self.path).joinpath(directory_value.value)
-
-        session = Session.object_session(self)
-
-        wait_time = 10
-        blocked = True
-        while blocked:
-            print('{} Archiving blocked...'.format(datetime.now()), flush=True)
-            session.refresh(self)
-
-            if directory_value in self.directories_archiving:
-                sleep(wait_time)
-            else:
-                try:
-                    self.directories_archiving.append(directory_value)
-                    session.commit()
-                    blocked = False
-
-                except IntegrityError:
-                    # Another process has already set `directories_archiving`
-                    session.rollback()
-                    sleep(wait_time)
-
-                except OperationalError:
-                    # Database is likely locked.
-                    session.rollback()
-                    sleep(wait_time)
-
-                if not blocked:
-                    # Need to ensure the block is released if copying fails:
-                    try:
-                        self._copy(src_dir, dst_dir, exclude)
-                    except shutil.Error as err:
-                        print('Archive copying error: {}'.format(err))
-
-                    self.directories_archiving.remove(directory_value)
-                    session.commit()
-
-    def _copy(self, src_dir, dst_dir, exclude):
-        """Do the actual copying.
-
-        TODO: does copytree overwrite all files or just copy
-        non-existing files?
-
-        """
-
-        # TODO: later (safely) copy the database to archive as well?
-        ignore = [CONFIG['hpcflow_directory']] + (exclude or [])
-
-        if ignore:
-            ignore_func = ignore_patterns(*ignore)
-        else:
-            ignore_func = None
-
-        start = datetime.now()
-        copytree_multi(str(src_dir), str(dst_dir), ignore=ignore_func)
-        end = datetime.now()
-        copy_seconds = (end - start).total_seconds()
-        print('Archive took {} seconds'.format(copy_seconds), flush=True)
 
 
 class IsCommandWriting(Base):
