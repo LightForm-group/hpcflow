765835da1cb30112b38d63cbe1a4590471f19e7e
aplowman <adam.jp@live.co.uk>
2019-08-30 23:47:32 +0100

    Add FILE_NAMES

--- hpcflow/__init__.py
+++ hpcflow/__init__.py
@@ -33,3 +33,8 @@ with _CONFIG_PATH.open('r') as handle:
 _VARS_LOOKUP_PATH = DATA_DIR.joinpath('_variable_lookup.yml')
 with _VARS_LOOKUP_PATH.open('r') as handle:
     VARS_LOOKUP = yaml.safe_load(handle)
+
+FILE_NAMES = {
+    'alt_scratch_exc_file': 'as_exclude',
+    'alt_scratch_exc_file_ext': '.txt',
+}
